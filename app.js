a5556c5c11b6cf976d2821cf078d04b3023acce1
dimas <dprcontractor@gmail.com>
2022-02-14 20:30:51 -0500

    ...

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const profileDataArgs = process.argv.slice(2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-    console.log('================');
+  console.log('================');
 
-    // is the same as this 
-    // is the same as this 
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-    
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
